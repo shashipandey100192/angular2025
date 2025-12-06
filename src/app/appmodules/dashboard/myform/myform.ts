bbde25179b2be0c20c5ff6baaef088ea921c7f59
shashi <shashiweb64@gmail.com>
2025-12-06 12:24:46 +0530

    form type

--- src/app/appmodules/dashboard/myform/myform.ts
+++ src/app/appmodules/dashboard/myform/myform.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ReactiveFormsModule,FormGroup,FormControl } from '@angular/forms';
+import { ReactiveFormsModule,FormGroup,FormControl,FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-myform',
@@ -10,13 +10,80 @@ import { ReactiveFormsModule,FormGroup,FormControl } from '@angular/forms';
 export class Myform {
 
 myformdata =new FormGroup({
-  fname:new FormControl(),
-  lname:new FormControl(),
+  fname:new FormControl(''),
+  lname:new FormControl(''),
 });
 
 mysubmit()
 {
-  console.log(this.myformdata.value);
+  console.log(this.myformdata);
+}
+
+updatevalue()
+{
+  this.myformdata.controls.fname.setValue("pppppppp");
+
+}
+
+
+
+//-----------------------nested form --------------------
+
+mynestform = new FormGroup({
+  fname:new FormControl(''),
+  lname:new FormControl(''),
+  
+  address: new FormGroup({
+    city:new FormControl(''),
+    phone:new FormControl(''),
+  })
+})
+
+mygroupform()
+{
+  console.log(this.mynestform.value)
+}
+
+
+updatenete()
+{
+  this.mynestform.patchValue({
+    fname:"dafsdfsd",
+    lname:"kumar",
+    address:{
+      city:"new delhi",
+      phone:"888888"
+    }
+  })
+}
+
+
+myformreset()
+{
+  this.mynestform.reset();
+}
+
+//--------------------------------------form builder -----------------------------
+mybulder:FormGroup
+
+constructor(private one : FormBuilder){
+  this.mybulder = one.group({
+    fname:[''],
+    lname:['']
+  })
+}
+
+printmyform()
+{
+  console.log(this.mybulder);
+}
+
+//------------------------------event handling --------------------------
+y:any;
+xyz(x:any)
+{
+  console.log(x);
+  this.y = x.data;
 }
 
 
